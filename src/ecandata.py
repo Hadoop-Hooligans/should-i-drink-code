23ad698c1bcbe02482e182b2800e27430657a03d
Jacob1461 <35879589+Jacob1461@users.noreply.github.com>
2024-06-04 12:31:17 +1200

    Moved across Ecan Data sourcing scripts

---
+++ src/ecandata.py
@@ -0,0 +1,40 @@
+"""Run this as a module"""
+
+import pandas as pd
+import requests
+
+#################
+import json
+
+import requests
+
+url = "https://gis.ecan.govt.nz/arcgis/rest/services/Public/Groundwater/MapServer/19/query?outFields=*&where=1%3D1&f=geojson"
+
+
+def get_ecan_data():
+    """Function to call the Ecan api. Returns a pandas dataframe"""
+    response = requests.get(url)
+    data = response.json()
+
+    extracted_data = []
+    for feature in data["features"]:
+        properties = feature["properties"]
+        longitude, latitude = feature["geometry"]["coordinates"]
+        extracted_data.append(
+            {
+                "Well_No": properties["Well_No"],
+                "Well_Status": properties["Well_Status"],
+                "Well_Type": properties["Well_Type"],
+                "Well_Owner": properties["Well_Owner"],
+                "Driller": properties["Driller"],
+                "Primary_Use": properties["Primary_Use"],
+                "Secondary_Use": properties["Second_Use"],
+                "WGS84_LONGITUDE": longitude,
+                "WGS84_LATITUDE": latitude,
+                "Link": properties["Link"],
+                "Locality": properties["Locality"],
+                "Street": properties["Street"],
+            }
+        )
+    df = pd.DataFrame(extracted_data)
+    return df
---
+++ src/ecanmain.py
@@ -0,0 +1,26 @@
+"""Main script for Sourcing the Ecan Data, Run as a module, will not work if run directly"""
+
+from .ecanwrangle import clean_ecan
+from .ecandata import get_ecan_data
+from .downloadCsv import download_excel_workbook
+
+
+def create_water_reports() -> None:
+    """Query Ecan API and build up a dataframe of Wells, then create a list of well links and ids and iteratively
+    Scrap the data on each well from the Ecan website. Creates a folder of csvs one for each well
+    """
+    all_ecan_data = get_ecan_data()
+    all_ecan_data.drop_duplicates(subset="Well_No", keep="first", inplace=True)
+    # Prints all the data from the API
+    # wrangled_ecan_data = clean_ecan(all_ecan_data)
+    all_ecan_data.to_csv("/app/src/ecan_data.csv")
+
+    codes = all_ecan_data["Well_No"].tolist()
+    print(len(codes))
+    download_excel_workbook(codes)
+    print("Finished Downloading")
+
+
+# ids = ["BW24/0039", "K38/0088", "M35_6639", "L35_0558"]
+# download_excel_workbook(ids)
+# create_water_reports()
